25ec27291d39c5a749a13f7b55ac473a6384d3d4
Batu Gülseçgin <batu.glsckn@gmail.com>
2026-05-13 14:23:49 +0300

    Runner file updated.

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         glue = "stepdefinitions", // Java kodlarının (step defs) dizin yolu
 
         tags = "@edge", // Sadece bu tag'e sahip senaryoları çalıştırır
-                       // Kullanılabilir tag'ler; @smoke , @e2e , @cart , @negative .
+                       // Kullanılabilir tag'ler; @smoke , @e2e , @cart , @negative , @edge
 
         dryRun = false // Eksik adım olup olmadığını kontrol etmek için true, testi gerçekten koşmak için false olmalıdır
 )
